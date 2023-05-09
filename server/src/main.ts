0d44ff4f7249bcf98d9dac49955dc081c25e3d1b
Aziz <aziz.abdurahimov151002@gmail.com>
2023-05-09 22:58:38 +0500

    feat: enable proxy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import * as express from 'express';
+import { INestApplication } from '@nestjs/common';
+
+function setupProxy(app: INestApplication) {
+  const proxyOptions = {
+    target: 'https://reviewly-server.onrender.com',
+    changeOrigin: true,
+  };
+
+  app.use('/api', createProxyMiddleware(proxyOptions));
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  setupProxy(app);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
