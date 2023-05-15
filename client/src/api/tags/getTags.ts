7331f1f2762059af1390800b17d2ccf06fca2c26
Aziz <aziz.abdurahimov151002@gmail.com>
2023-05-15 19:13:30 +0500

    fix: render more tags

--- client/src/api/tags/getTags.ts
+++ client/src/api/tags/getTags.ts
@@ -1,7 +1,7 @@
 import axiosInstance from 'api/axios';
 import { Tag } from 'interfaces';
 
-export const getTags = async (limit = 10) => {
+export const getTags = async (limit = 100) => {
   const { data } = await axiosInstance.get<Tag[]>('/tags', {
     params: {
       limit,
